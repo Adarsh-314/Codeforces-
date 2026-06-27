5fc2dad053fa779f48f2091bd738ec4d203f8768
Adarsh-314 <adarshp.ug25.me@nitp.ac.in>
2026-06-27 15:32:28 +0530

    Add Army [A] from Codeforces

---
+++ 38/A - Army/solution.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include<vector>
+using namespace std;
+int main() {
+ios_base:: sync_with_stdio(false);
+cin.tie(NULL);
+    int n;
+    cin>>n;
+    int sum=0;
+    vector<int>vec(n-1);
+    int a,b;
+     for(int i=0; i<n-1; i++) {
+         cin>>vec[i];
+     }
+    
+ 
+     cin>>a>>b;
+ 
+     for (int i=a; i<b; i++) {
+        sum += vec[i-1];
+     }
+     cout << sum << endl;
+return 0;
+}
