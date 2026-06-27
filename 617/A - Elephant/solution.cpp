aadf6901e797d43ca28bc91bf120f3021ea2f1f0
Adarsh-314 <adarshp.ug25.me@nitp.ac.in>
2026-06-27 15:36:10 +0530

    Add Elephant [A] from Codeforces

---
+++ 617/A - Elephant/solution.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() {
+ios_base:: sync_with_stdio(false);
+cin.tie(NULL);
+    int x;
+    cin>>x;
+    if(x<=5) cout<<1<<endl;
+    else if(x%5==0) cout<<(x/5)<<endl;
+    else cout<<(x/5)+1<<endl;
+    
+return 0;
+}
