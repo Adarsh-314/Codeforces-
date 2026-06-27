bc18766cc6b8da14fce08c3d3e5d25ecfa0151be
Adarsh-314 <adarshp.ug25.me@nitp.ac.in>
2026-06-27 15:33:19 +0530

    Add Games on the Train [A] from Codeforces

---
+++ 2236/A - Games on the Train/solution.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+void solve() {
+    int n;
+    cin>>n;
+    vector<int>h(n);
+    for (int i=0; i<n; i++) {
+        cin>>h[i];
+    }
+    sort(h.begin(), h.end());
+    cout<<((h[n-1]-h[0])+1)<<endl;
+ 
+}
+int main() {
+ios_base:: sync_with_stdio(false);
+cin.tie(NULL);
+int t;
+cin>>t;
+while (t--) {
+ 
+    solve ();
+}
+    
+return 0;
+}
