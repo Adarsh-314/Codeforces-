f1d8ba9d1854e0447a65f97462ca13879703e141
Adarsh-314 <adarshp.ug25.me@nitp.ac.in>
2026-06-27 15:35:40 +0530

    Add Circle of Apple Trees [A] from Codeforces

---
+++ 2153/A - Circle of Apple Trees/solution.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+#include <set>
+using namespace std;
+int main() {
+ios_base:: sync_with_stdio(false);
+cin.tie(NULL);
+int t;
+cin>>t;
+while (t--) {
+    int  n;
+    set<int> s;
+    cin>>n;
+    for (int i = 0; i < n; i++) {
+        int x;
+        cin >> x;
+        s.insert(x);
+    }
+    cout<<s.size()<<endl;
+}
+    
+return 0;
+ 
+}
