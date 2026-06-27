9b76c6788ed564f135feda8e65d50c28e43b4385
Adarsh-314 <adarshp.ug25.me@nitp.ac.in>
2026-06-27 15:29:26 +0530

    Add Sakurako's Exam [A] from Codeforces

---
+++ 2008/A - Sakurako's Exam/solution.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int t;
+    cin>>t;
+    while(t--) {
+        int a,b;
+        cin>>a>>b;
+        if((a%2==0 && b%2== 0  ) || (a%2== 0 && b%2 != 0) && a>0 && b>0)
+        {
+            cout<<"YES
+";
+        }
+        else cout<<"NO
+";
+    }
+return 0;
+}
