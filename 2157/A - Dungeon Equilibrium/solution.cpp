69f8b986c2be2b65fba732822e0dede23aca642b
Adarsh-314 <adarshp.ug25.me@nitp.ac.in>
2026-06-27 15:35:28 +0530

    Add Dungeon Equilibrium [A] from Codeforces

---
+++ 2157/A - Dungeon Equilibrium/solution.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+ 
+using namespace std;
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int t;
+    cin >> t;
+    while (t--) {
+           int n;
+    cin >> n;
+    
+    vector<int> vec(n);
+    for (int i = 0; i < n; i++) {
+        cin >> vec[i];
+    }
+    sort(vec.begin(), vec.end());
+    
+    long long deletions = 0;
+    int i = 0;
+    while (i < n) {
+        int current_val = vec[i];
+        int count = 0;
+        while (i < n && vec[i] == current_val) {
+            count++;
+            i++; 
+        }
+        if (current_val == 0) {
+            deletions += count;
+        } else if (count < current_val) {
+            deletions += count;
+        } else {
+            deletions += (count - current_val);
+        }
+    }
+    
+    cout << deletions << "
+";
+    }
+    return 0;
+}
