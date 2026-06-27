42fe99c369a356e0cae804379f73b2a6b9de2783
Adarsh-314 <adarshp.ug25.me@nitp.ac.in>
2026-06-27 15:29:13 +0530

    Add In Search of an Easy Problem [A] from Codeforces

---
+++ 1030/A - In Search of an Easy Problem/solution.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int count=0;
+    int n,i;
+    cin>>n;
+    int a[n];
+    for (i=0; i<n; i++) {
+        cin>>a[i];
+    }
+    for (i=0; i<n; i++) {
+        if (a[i]==1) {
+            count++;
+           
+        }
+    }
+    if (count>0) {
+        cout<<"HARD";
+    }
+    else {
+        cout<<"EASY";
+    }
+    return 0;
+}   
+ 
+ 
