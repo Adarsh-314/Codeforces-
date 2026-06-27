d50a3914d21cb790511160d230390f44e25d1a01
Adarsh-314 <adarshp.ug25.me@nitp.ac.in>
2026-06-27 15:35:24 +0530

    Add Suspension [A] from Codeforces

---
+++ 2158/A - Suspension/solution.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+int main() {
+ios_base:: sync_with_stdio(false);
+cin.tie(NULL);
+    int t;
+    cin>>t;
+    while (t--) {
+        int n;
+        cin>>n;
+        int y,r;
+        cin>>y>>r;
+        if (r>=n) {
+            cout<<n<<endl;
+        }
+        else if (y/2>=n) {
+            cout<<n<<endl;
+        }
+        else if (r+y/2>=n){
+            cout<<n<<endl;
+ 
+        }
+        else {
+            cout<<r+y/2<<endl;
+        }
+    }
+return 0;
+}
