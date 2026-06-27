541938ff6ec19a653e1857f46e697e7b8a7bf258
Adarsh-314 <adarshp.ug25.me@nitp.ac.in>
2026-06-27 15:30:03 +0530

    Add Square Difference [B] from Codeforces

---
+++ 1033/B - Square Difference/solution.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+bool isPrime(long long n) {
+    if (n == 1) return false;            
+    for (long long i=2; i*i<= n; i++) {
+        if (n % i == 0)
+            return false;
+    }
+    return true;
+}
+int main() {
+ios_base:: sync_with_stdio(false);
+cin.tie(NULL);
+    int t;
+    cin>>t;
+    while (t--) {
+      int cnt=0;
+        
+        long long a,b;
+        cin>>a>>b;
+        if ((a-b)==1 && isPrime(a+b)) {
+             cout<<"YES
+";
+        }
+        else cout<<"NO
+";
+        
+    }
+return 0;
+}
