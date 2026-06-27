a8a72c4ec532a4b66749af7473097e19ee20eb6a
Adarsh-314 <adarshp.ug25.me@nitp.ac.in>
2026-06-27 15:29:57 +0530

    Add Odd Divisor [A] from Codeforces

---
+++ 1475/A - Odd Divisor/solution.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        long long n;
+        cin>>n;
+        int cnt= 0;
+       while(n%2==0) {
+        n=n/2;
+       }
+       if (n==1) {
+        cout<<"NO
+";
+       }
+       else cout<<"YES
+";
+    }
+return 0;
+}
