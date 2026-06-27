d82a01389cb124d31e8b307652f022de4b4e7c1b
Adarsh-314 <adarshp.ug25.me@nitp.ac.in>
2026-06-27 15:31:47 +0530

    Add DBMB and the Array [A] from Codeforces

---
+++ 2193/A - DBMB and the Array/solution.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int t;
+    cin >> t;
+    
+    while (t--) {
+        int n, s, x;
+        cin >> n >> s >> x;
+        
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            int a;
+            cin >> a;
+            sum += a;
+        }
+        
+        int diff = s - sum;
+        
+        if (diff < 0) {
+            cout << "NO
+";
+        } else if (diff % x == 0) {
+            cout << "YES
+";
+        } else {
+            cout << "NO
+";
+        }
+    }
+    
+    return 0;
+}
